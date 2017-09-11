906f5a77436fc67dc291b90fabd176ed2bc8a5f9
DOOMer <doomer3d@gmail.com>
2017-09-11 17:11:19 +0300

    Add date converter class

---
+++ app/src/main/java/org/doomer/qnotez/db/DateConverter.java
@@ -0,0 +1,17 @@
+package org.doomer.qnotez.db;
+
+import android.arch.persistence.room.TypeConverter;
+import java.util.Date;
+
+public class DateConverter {
+
+    @TypeConverter
+    public static Date toDate(Long timestamp) {
+        return timestamp == null ? null : new Date(timestamp);
+    }
+
+    @TypeConverter
+    public static long toTimestamp(Date date) {
+        return date == null ? 0 : date.getTime();
+    }
+}
