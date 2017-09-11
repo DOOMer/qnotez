3088a5f03977a0d9750f31e97b8c614d0c5aa13c
DOOMer <doomer3d@gmail.com>
2017-09-11 19:11:31 +0300

    Add DAO interface for Note model class

---
+++ app/src/main/java/org/doomer/qnotez/db/NoteDao.java
@@ -0,0 +1,29 @@
+package org.doomer.qnotez.db;
+
+import android.arch.lifecycle.LiveData;
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.Delete;
+import android.arch.persistence.room.Query;
+import android.arch.persistence.room.TypeConverters;
+
+import java.util.List;
+
+import static android.arch.persistence.room.OnConflictStrategy.REPLACE;
+
+@Dao
+@TypeConverters(DateConverter.class)
+public interface NoteDao {
+
+    @Query("SELECT * FROM " + NoteModel.TABLE_NAME)
+    LiveData<List<NoteModel>> getAllItems();
+
+    @Query("SELECT * FROM " + NoteModel.TABLE_NAME + " WHERE id = :id")
+    LiveData<NoteModel> getItem(String id);
+
+    @Insert(onConflict = REPLACE)
+    void addItem(NoteModel item);
+
+    @Delete
+    void deleteItem(NoteModel item);
+}
