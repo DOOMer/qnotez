74142f03d47826d4ca0b1e98ff43c29025e4dad2
DOOMer <doomer3d@gmail.com>
2017-09-22 15:52:07 +0300

    Add util functions for get info about version number and build date-time

---
+++ app/src/main/java/org/doomer/qnotez/utils/TextUtils.java
@@ -0,0 +1,41 @@
+package org.doomer.qnotez.utils;
+
+import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.doomer.qnotez.App;
+import org.doomer.qnotez.BuildConfig;
+
+public class TextUtils {
+
+    public static String getVersionString() {
+        String versionString;
+
+        Context appContext = App.getAppContext();
+
+        try {
+            PackageInfo packageInfor = appContext.getPackageManager().getPackageInfo(
+                    appContext.getPackageName(), 0
+            );
+            versionString = packageInfor.versionName;
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+            versionString = "Unknown";
+        }
+
+        return versionString;
+    }
+
+    public static String getBuildTime() {
+        Date buildDate = new Date(BuildConfig.TIMESTAMP);
+
+        SimpleDateFormat sdf = new SimpleDateFormat("dd MMM yyyy k:mm");
+        String buildString = sdf.format(buildDate);
+
+        return buildString;
+    }
+}
